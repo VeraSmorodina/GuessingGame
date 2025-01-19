93351c331c25d7a3957397e2736fb64e668ec634
Крайнюков Сергей <sergey.kraynyukov@gazprombank.ru>
2025-01-19 20:48:51 +0300

    .

--- app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/hfad/guessinggame/ResultFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hfad.guessinggame.databinding.FragmentResultBinding
 import androidx.navigation.findNavController
 import androidx.lifecycle.ViewModelProvider
 import androidx.compose.material.MaterialTheme
@@ -19,18 +18,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.platform.ComposeView
 
 class ResultFragment : Fragment() {
-    private var _binding: FragmentResultBinding? = null
-    private val binding get() = _binding!!
     lateinit var viewModel: ResultViewModel
     lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentResultBinding.inflate(inflater, container, false).apply {
-            composeView.setContent {
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this@ResultFragment, viewModelFactory)
+            .get(ResultViewModel::class.java)
+
+
+        return ComposeView(requireContext()).apply {
+            setContent {
                 MaterialTheme {
                     Surface {
                         view?.let { ResultFragmentContent(it, viewModel) }
@@ -38,40 +42,35 @@ class ResultFragment : Fragment() {
                 }
             }
         }
-        val view = binding.root
-        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
-        viewModelFactory = ResultViewModelFactory(result)
-        viewModel = ViewModelProvider(this, viewModelFactory)
-            .get(ResultViewModel::class.java)
-        binding.resultViewModel = viewModel
+    }
 
-        binding.newGameButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_resultFragment_to_gameFragment)
-        }
-        return view
-    }
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 
     @Composable
     fun ResultText(result: String) {
-        Text(text = result,
+        Text(
+            text = result,
             fontSize = 28.sp,
-            textAlign = TextAlign.Center)
+            textAlign = TextAlign.Center
+        )
     }
+
     @Composable
     fun NewGameButton(clicked: () -> Unit) {
         Button(onClick = clicked) {
             Text("Start New Game")
         }
     }
+
     @Composable
     fun ResultFragmentContent(view: View, viewModel: ResultViewModel) {
-        Column(modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             ResultText(viewModel.result)
             NewGameButton {
                 view.findNavController()
